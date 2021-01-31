ef9bd6a0c82b2aecd3316b5d04a96a7d77ddfeef
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2021-01-31 18:09:12 +0200

    implement number rows

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,3 +1,22 @@
-const Calculator = () => <h1>Calculator</h1>;
+const rows = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [0]];
+
+const Calculator = () => {
+  return (
+    <div className="calculator">
+      <h1>Calculator</h1>
+      <div role="grid">
+        {rows.map((row) => {
+          return (
+            <div key={row.toString()} role="row">
+              {row.map((n) => (
+                <button key={n}>{n.toString()}</button>
+              ))}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Calculator;
