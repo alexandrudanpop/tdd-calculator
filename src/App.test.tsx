8c4e34afeb5c7ca722d7dbaa794282feb9d18bf3
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2021-01-31 17:28:40 +0200

    write test to show calculator

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders calculator", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const calculatorElement = screen.getByText(/calculator/i);
+  expect(calculatorElement).toBeInTheDocument();
 });
