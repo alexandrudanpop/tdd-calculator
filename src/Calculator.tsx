f7df06cf729dd37747176012c173f2551dee2d8a
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2021-01-31 18:31:43 +0200

    implement operators

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,19 +1,30 @@
-const rows = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [0]];
+import { Fragment } from "react";
 
+const rows = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [0]];
+const calcOperators = ["+", "-", "×", "÷"];
+const equalSign = "=";
+const clear = "C";
 const Calculator = () => {
   return (
     <div className="calculator">
       <h1>Calculator</h1>
       <div role="grid">
-        {rows.map((row) => {
+        {rows.map((row, i) => {
           return (
-            <div key={row.toString()} role="row">
-              {row.map((n) => (
-                <button key={n}>{n.toString()}</button>
-              ))}
-            </div>
+            <Fragment key={row.toString()}>
+              <div role="row">
+                {i === 3 && <button>{clear}</button>}
+                {row.map((n) => (
+                  <button key={n}>{n}</button>
+                ))}
+                {i === 3 && <button>{equalSign}</button>}
+              </div>
+            </Fragment>
           );
         })}
+        {calcOperators.map((c) => (
+          <button key={c}>{c.toString()}</button>
+        ))}
       </div>
     </div>
   );
