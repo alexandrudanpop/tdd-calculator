29824cee04a3fec40e915c6277b4f1dad0e38fda
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2021-01-31 18:40:09 +0200

    implement input

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,13 +1,20 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 const rows = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [0]];
 const calcOperators = ["+", "-", "×", "÷"];
 const equalSign = "=";
 const clear = "C";
 const Calculator = () => {
+  const [value, setValue] = useState("");
   return (
     <div className="calculator">
       <h1>Calculator</h1>
+      <input
+        type="text"
+        defaultValue={value}
+        placeholder="calculate"
+        disabled
+      />
       <div role="grid">
         {rows.map((row, i) => {
           return (
