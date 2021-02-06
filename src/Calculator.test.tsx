a4e2d4bae531d86e24285e4c071ea25a3a1e4812
alexandrudanpop <alexandrudanpop@gmail.com>
2021-02-06 18:00:48 +0200

    test for clear

--- src/Calculator.test.tsx
+++ src/Calculator.test.tsx
@@ -133,4 +133,24 @@ describe("<Calculator />", () => {
       }).value
     ).toBe("5.8");
   });
+
+  it("can clear results", async () => {
+    render(<Calculator />);
+    const one = screen.getByText("1");
+    const two = screen.getByText("2");
+    const plus = screen.getByText("+");
+    const clear = screen.getByText("C");
+    fireEvent.click(one);
+    fireEvent.click(plus);
+    fireEvent.click(two);
+
+    fireEvent.click(clear);
+
+    const result = await screen.findByPlaceholderText("calculate");
+    expect(
+      (result as HTMLElement & {
+        value: string;
+      }).value
+    ).toBe("");
+  });
 });
